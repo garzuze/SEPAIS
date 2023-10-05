00671776b5357a5995fc319c646a8df2b1dc4af2
Pietro F. A. Sotile <126175031+PietroSotile@users.noreply.github.com>
2023-10-05 19:30:03 -0300

    Update popular_tabelas.sql
    Inserido mais registros nas tabelas de liberação e recado

--- popular_tabelas.sql
+++ popular_tabelas.sql
@@ -31,6 +31,8 @@ INSERT INTO `sepaisdb`.`sepae` (`username`, `nome`, `email_institucional`, `senh
 -- inserir recado
 INSERT INTO `sepaisdb`.`recado` (`titulo`, `recado`, `data`, `validade`, `sepae_username`) VALUES ('Reunião do Pais', 'A reunião dos pais vai acontecer nesta sexta-feira', '2023-10-05', '2023-10-14', 'Neras');
 INSERT INTO `sepaisdb`.`recado` (`titulo`, `recado`, `data`, `validade`, `sepae_username`) VALUES ('Semana Científica', 'A semana científica ira acontecer dos dias 11/10 a 13/10. Se divirta!', '2023-10-6', '2023-10-14', 'Chicão');
+INSERT INTO `sepaisdb`.`recado` (`id`, `titulo`, `recado`, `data`, `sepae_username`) VALUES ('3', 'Boas Vindas', 'Boas vindas ao Instituto Federal', '2023-02-15', 'Neras');
+INSERT INTO `sepaisdb`.`recado` (`titulo`, `recado`, `data`, `validade`, `sepae_username`) VALUES ('Rematrícula', 'Realize a rematrícula o quanto antes', '2023-02-15', '2023-02-31', 'Chicão');
 
 -- inserir responsaveis
 INSERT INTO `sepaisdb`.`responsavel` (`email`, `nome`, `senha`, `cpf`) VALUES ('jailson.mendes@gmail.com', 'Jailson Mendes', '$hash', '12345678912');
@@ -39,7 +41,14 @@ INSERT INTO `sepaisdb`.`responsavel` (`email`, `nome`, `senha`, `cpf`) VALUES ('
 -- inserir responsavel_libera_aluno
 INSERT INTO `sepaisdb`.`responsavel_libera_aluno` (`responsavel_email`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('jailson.mendes@gmail.com', '2', '2023-10-11', 'Luto');
 INSERT INTO `sepaisdb`.`responsavel_libera_aluno` (`responsavel_email`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('rafael.greca@gmail.com', '4', '2023-10-06', 'Transporte');
+INSERT INTO `sepaisdb`.`responsavel_libera_aluno` (`responsavel_email`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('jailson.mendes@gmail.com', '1', '2023-10-05', 'Médico');
+INSERT INTO `sepaisdb`.`responsavel_libera_aluno` (`responsavel_email`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('rafael.greca@gmail.com', '5', '2023-10-05', 'Motivo particular');
+INSERT INTO `sepaisdb`.`responsavel_libera_aluno` (`responsavel_email`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('rafael.greca@gmail.com', '3', '2023-10-06', 'Mal-estar');
 
 -- inserir sepae_libera_aluno
 INSERT INTO `sepaisdb`.`sepae_libera_aluno` (`sepae_username`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('Chicão', '2', '2023-10-11', 'Professor faltou');
 INSERT INTO `sepaisdb`.`sepae_libera_aluno` (`sepae_username`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('Neras', '1', '2023-10-06', 'Professor faltou');
+INSERT INTO `sepaisdb`.`sepae_libera_aluno` (`sepae_username`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('Neras', '3', '2023-10-05', 'Professor faltou');
+INSERT INTO `sepaisdb`.`sepae_libera_aluno` (`sepae_username`, `aluno_id`, `data`, `motivo_motivo`, `horario_saida`) VALUES ('Chicão', '4', '2023-10-05', 'Professor faltou', '0');
+INSERT INTO `sepaisdb`.`sepae_libera_aluno` (`sepae_username`, `aluno_id`, `data`, `motivo_motivo`) VALUES ('Chicão', '5', '2023-10-05', 'Professor faltou');
+
