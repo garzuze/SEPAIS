16cc133a1be41fe007765fcbe51db0d2084b1afa
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2023-10-11 09:12:56 -0300

    Primeira parte front-end
    Estamos bem no início, por enquanto só tem um header com informações sobre o servidor.

---
+++ frontend/index.php
@@ -0,0 +1,66 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard | SEPAIS</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        function updateTime() {
+            var currentTime = new Date()
+            var hours = currentTime.getHours()
+            var minutes = currentTime.getMinutes()
+
+            if (hours < 10) {
+                hours = "0" + hours
+            }
+
+            if (minutes < 10) {
+                minutes = "0" + minutes
+            }
+
+            var time = hours + ":" + minutes;
+            document.getElementById('time').innerHTML = time;
+        }
+        setInterval(updateTime, 1000);
+    </script>
+</head>
+
+<body>
+    <nav class="nav-top w-full h-16 bg-[#040401] grid grid-cols-3 justify-items-center content-center">
+        <div class="user ">
+            <div class="flex items-center md:order-2">
+                <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-[#00bf63]" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                    <span class="sr-only">Abrir menu de usuário</span>
+                    <img class="w-8 h-8 rounded-full" src="static/nereu.jpg" alt="user photo">
+                </button>
+                <!-- Dropdown menu -->
+                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow" id="user-dropdown">
+                    <div class="px-4 py-3">
+                        <span class="block text-sm text-gray-900">Nereu da Silva Filho</span>
+                        <span class="block text-sm  text-gray-500 truncate">nereu.filho@ifpr.edu.br</span>
+                    </div>
+                    <ul class="py-2" aria-labelledby="user-menu-button">
+                        <li>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="">
+            <p class="text-white" id="time"></p>
+        </div>
+        <div class="">
+            <img src="static/sepais_logo.png" class="sm:h-6 h-4">
+        </div>
+    </nav>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+</body>
+
+</html>
