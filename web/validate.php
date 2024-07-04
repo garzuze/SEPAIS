ff05881eee1e6925bd0b5c609404b733a647fac2
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2024-07-04 12:18:02 -0300

    Apagar pasta "web"
    Não será necessário manter uma pasta com uma cópia da forma que o sistema está na web, vamos padronizar.

--- web/validate.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include('secure.php');
-if (isset($_POST['submit'])){
-    $email = $_POST['email'];
-    $password = $_POST['senha'];
-    verify_user($email, $password);
-}
-?>
