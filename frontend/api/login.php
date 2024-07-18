956451d7b66e8ab4e1474b5670b62a3a3b651114
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2024-07-18 17:17:34 -0300

    Consertar erros com queries mysql

--- frontend/api/login.php
+++ frontend/api/login.php
@@ -2,18 +2,18 @@
 header('Content-type: application/json');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!empty($_POST["email"]) && !empty($_POST["password"])) {
+        require_once("../connect.php");
+        $mysqli = connect();
         $email = $_POST["email"];
         $password = $_POST["password"];
         try {
             $SELECT_USER_SQL = "SELECT * FROM `usuario` WHERE email = ?";
-            $mysqli = connect();
             $select_user_stmt = $mysqli->prepare($SELECT_USER_SQL);
             $select_user_stmt->bind_param("s", $email);
             $select_user_stmt->execute();
-            $user_flag = $select_user_stmt->num_rows();
-            if ($user_flag > 0) {
-                $user_data = $select_user_stmt->get_result();
-                $user_data = $user_data->fetch_all(MYSQLI_ASSOC);
+            $user_flag = $select_user_stmt->get_result();
+            if ($user_flag->num_rows > 0) {
+                $user_data = $user_flag->fetch_all(MYSQLI_ASSOC);
                 if (password_verify($password, $user_data[0]['senha'])) {
                     $user_object = array(
                         "email" => $user_data[0]["email"],
--- frontend/api/signup.php
+++ frontend/api/signup.php
@@ -2,19 +2,20 @@
 header("Content-type: application/json");
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (!empty($_POST["email"]) && !empty($_POST["name"]) && !empty($_POST["password"])) {
+        require_once("../connect.php");
+        $mysqli = connect();
         $email = $_POST["email"];
         $name = $_POST["name"];
         $password = $_POST["password"];
+        $photo_path = "static/default_user_icon.jpg";
         try {
-            require("../connect.php");
             // Verificar se não é um usuário duplicado
-            $SELECT_USER_SQL = "SELECT * FROM `usuario` WHERE email = ?";
-            $mysqli = connect();
+            $SELECT_USER_SQL = "SELECT * FROM usuario WHERE email = ?";
             $duplicate_user_stmt = $mysqli->prepare($SELECT_USER_SQL);
             $duplicate_user_stmt->bind_param("s", $email);
             $duplicate_user_stmt->execute();
-            $duplicate_user_flag = $duplicate_user_stmt->num_rows();
-            if ($duplicate_user_flag > 0) {
+            $duplicate_user_flag = $duplicate_user_stmt->get_result();
+            if ($duplicate_user_flag->num_rows > 0) {
                 http_response_code(404);
                 $server_response_error = array(
                     "code" => http_response_code(404),
@@ -26,9 +27,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 // Inserir usuário
                 // Encriptar senha
                 $hashed_pwd = password_hash($password, PASSWORD_DEFAULT);
-                $INSERT_USER_SQL = "INSERT INTO `usuario` (`email`, `nome`, `senha`, `foto_path`) VALUES (?, ?, ?, '../static/default_user_icon.jpg'";
+                $INSERT_USER_SQL = "INSERT INTO `usuario` (`email`, `nome`, `senha`, `foto_path`) VALUES (?, ?, ?, ?)";
                 $insert_user_stmt = $mysqli->prepare($INSERT_USER_SQL);
-                $insert_user_stmt->bind_param("sss", $email, $name, $hashed_pwd);
+                $insert_user_stmt->bind_param("ssss", $email, $name, $hashed_pwd, $photo_path);
                 if ($insert_user_stmt->execute()) {
                     $server_response_success = array(
                         "code" => http_response_code(200),
@@ -61,6 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             "code" => http_response_code(404),
             "status" => false,
             "message" => "Parâmetros inválidos.",
+            "data"=>$_POST,
         );
         echo json_encode($server_response_error);
     }
