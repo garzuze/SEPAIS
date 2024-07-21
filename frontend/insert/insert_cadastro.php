0f710dd65df698ab4e427a8ce002159b97c1697e
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2024-07-21 10:53:25 -0300

    Mudar forma de inserir cadastro

--- frontend/insert/insert_cadastro.php
+++ frontend/insert/insert_cadastro.php
@@ -1,41 +1,32 @@
 <?php
 include('../secure.php');
-if (isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
-// Preparando variáveis para inserção no BD
-$funcao = $_POST['funcao'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-$hashed_password = password_hash($senha, PASSWORD_DEFAULT);
-$foto_path = 'nenhum';
-// Conectando ao BD e inserindo novos dados
-$sql = connect();
+    // Preparando variáveis para inserção no BD
+    $funcao = $_POST['funcao'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $hashed_password = password_hash($senha, PASSWORD_DEFAULT);
+    $foto_path = "static/default_user_icon.jpg";
+    $sql = connect();
 
-try {
-    $query = $sql->prepare("INSERT INTO usuario (nome, email, senha, foto_path) VALUES (?, ?, ?, ?)");
-    $query->bind_param("ssss", $name, $email, $hashed_password, $foto_path);
-    $query->execute();
+    try {
+        $insert_user_query = $sql->prepare("INSERT INTO usuario (nome, email, senha, foto_path) VALUES (?, ?, ?, ?)");
+        $insert_user_query->bind_param("ssss", $name, $email, $hashed_password, $foto_path);
+        $insert_user_query->execute();
 
-    if ($funcao == 'portaria'){
-        $query = $sql->prepare("INSERT INTO portaria (email) VALUES (?)");
-        $query->bind_param("s",$email);
-        $query->execute();
-    } elseif ($funcao == 'sepae'){
-        $query = $sql->prepare("INSERT INTO sepae (email) VALUES (?)");
-        $query->bind_param("s",$email);
-        $query->execute();
-    }
-
-header('Location: ../login.php');
-} catch (mysqli_sql_exception $e) {
-    if ($e->getCode() == 1062) {
-        header("Location: ../signup.php?erro=".$e->getMessage()."");
-    } else {
-        header('Location: ../signup.php?erro='.$e->getMessage().'');
-        throw $e;// in case it's any other error
+        $insert_child_user_stmt = "INSERT INTO " . $funcao . " (email) VALUES (?)";
+        $insert_child_user_query = $sql->prepare($insert_child_user_stmt);
+        $insert_child_user_query->bind_param("s", $email);
+        $insert_child_user_query->execute();
+        header('Location: ../login.php');
+    } catch (mysqli_sql_exception $e) {
+        if ($e->getCode() == 1062) {
+            header("Location: ../signup.php?erro=" . $e->getMessage() . "");
+        } else {
+            header('Location: ../signup.php?erro=' . $e->getMessage() . '');
+            throw $e; // in case it's any other error
+        }
     }
 }
-}
-
-?>
