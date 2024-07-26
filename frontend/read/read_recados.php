e831a7043214a0663c30b9eaeb1c693930f2e717
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2024-07-26 11:21:35 -0300

    Criar API de recados

--- frontend/read/read_recados.php
+++ frontend/read/read_recados.php
@@ -1,27 +1,33 @@
 <?php
 require_once('../connect.php');
 
-session_start();
-if(isset($_SESSION['email'])) {
-try {
-	$mysqli = connect();
-	$consulta = $mysqli->prepare("SELECT id, titulo, recado, data, validade, sepae_email from recado 
-    where (CURDATE() < validade) or (validade is null) ORDER BY id DESC;");
-	$consulta->execute();
+function read_recados()
+{
+	try {
+		$mysqli = connect();
+		$consulta = $mysqli->prepare("SELECT id, titulo, recado, data, validade, sepae_email from recado 
+		where (CURDATE() < validade) or (validade is null) ORDER BY id DESC;");
+		$consulta->execute();
 
-	$resultado = $consulta->get_result();
-	$resultadoFormatado = $resultado->fetch_all(MYSQLI_ASSOC);
-} catch (Exception $e) {
-	error_log($e->getMessage());
-	print_r($mysqli->error);
-	exit('Alguma coisa estranha aconteceu...');
+		$resultado = $consulta->get_result();
+		$resultadoFormatado = $resultado->fetch_all(MYSQLI_ASSOC);
+
+		$consulta->close();
+		$mysqli->close();
+		return $resultadoFormatado;
+	} catch (Exception $e) {
+		error_log($e->getMessage());
+		print_r($mysqli->error);
+		exit('Alguma coisa estranha aconteceu...');
+	}
 }
 
-echo json_encode($resultadoFormatado,  JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+session_start();
+if (isset($_SESSION['email'])) {
 
-$consulta->close();
-$mysqli->close();
-} else{
+	$recados = read_recados();
+
+	echo json_encode($recados,  JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+} else {
 	echo json_encode(0);
 }
-?>
