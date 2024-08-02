5686b31d3a57096a880c78fd9ab3770aa26de743
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2024-08-02 07:56:28 -0300

    Criar API que retorna dependentes baseado no token

---
+++ frontend/api/get_dependentes.php
@@ -0,0 +1,46 @@
+<?php
+ini_set("display_errors", 1);
+require_once('../connect.php');
+require "../../vendor/autoload.php";
+require "../read/read_alunos_responsaveis.php";
+
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+header('Content-type: application/json;');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	$all_headers = getallheaders();
+	$jwt = $all_headers['Authorization'];
+	$data = $_POST;
+	if (!empty($jwt)) {
+		try {
+			$secret = "SomosOsSepinhosBananaoDoChicao";
+			$decoded_data =  (array) JWT::decode($jwt, new Key($secret, 'HS256'));
+            $email_responsavel = $decoded_data["sub"];
+            $dependentes = read_dependentes($email_responsavel);
+			http_response_code(200);
+			echo json_encode(array(
+				"status" => True,
+				"message" => $dependentes,
+				"user_data" => $decoded_data,
+			));
+		} catch (Exception $exception) {
+			http_response_code(500);
+			echo json_encode(array(
+				"status" => False,
+				"message" => $exception->getMessage(),
+			));
+		}
+	} else {
+		echo "JWT não encontrado";
+	}
+} else {
+	http_response_code(404);
+	$server_response_error = array(
+		"code" => http_response_code(404),
+		"status" => false,
+		"message" => "Bad Request"
+	);
+	echo json_encode($server_response_error);
+}
