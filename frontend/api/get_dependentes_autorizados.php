3aae3be83709bb0bed17f71e6b98cf8a2d8bc41d
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2024-09-11 08:36:14 -0300

    Criar API para requisitar dependentes autorizados

---
+++ frontend/api/get_dependentes_autorizados.php
@@ -0,0 +1,48 @@
+<?php
+ini_set("display_errors", 1);
+date_default_timezone_set('America/Sao_Paulo');
+require_once('../connect.php');
+require "../../vendor/autoload.php";
+require "../read/read_alunos_autorizados.php";
+	
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+header('Content-type: application/json;');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	$date = date('Y-m-d');
+	$all_headers = getallheaders();
+	$jwt = $all_headers['Authorization'];
+	$data = $_POST;
+	if (!empty($jwt)) {
+		try {
+			$secret = "SomosOsSepinhosBananaoDoChicao";
+			$decoded_data =  (array) JWT::decode($jwt, new Key($secret, 'HS256'));
+            $email_responsavel = $decoded_data["sub"];
+            $dependentes_autorizados = read_alunos_autorizados($date, $email_responsavel);
+			http_response_code(200);
+			echo json_encode(array(
+				"status" => True,
+				"message" => $dependentes_autorizados,
+				"user_data" => $decoded_data,
+			));
+		} catch (Exception $exception) {
+			http_response_code(500);
+			echo json_encode(array(
+				"status" => False,
+				"message" => $exception->getMessage(),
+			));
+		}
+	} else {
+		echo "JWT não encontrado";
+	}
+} else {
+	http_response_code(404);
+	$server_response_error = array(
+		"code" => http_response_code(404),
+		"status" => false,
+		"message" => "Bad Request"
+	);
+	echo json_encode($server_response_error);
+}
---
+++ frontend/read/read_alunos_autorizados.php
@@ -0,0 +1,47 @@
+<?php
+require_once('../connect.php');
+
+function read_alunos_autorizados($date, $email)
+{
+    $mysqli = connect();
+    try {
+        $consulta = $mysqli->prepare("SELECT 
+        `aluno`.`id` AS `id_aluno`,
+        `aluno`.`nome` AS `nome_aluno`,
+        `turma`.`turma` AS `turma`,
+        `sepae_libera_aluno`.`data` AS `data`,
+        `sepae_libera_aluno`.`horario_saida` AS `horario_saida`,
+        `motivo`.`motivo` AS `motivo`
+    FROM    
+        (((`aluno`
+        JOIN `turma`)
+        JOIN `sepae_libera_aluno`)
+        JOIN `responsavel_has_aluno`
+        JOIN `motivo`)  
+    WHERE
+        ((`aluno`.`id` = `sepae_libera_aluno`.`aluno_id`)
+            and (`isAutorizado` = TRUE)
+            AND (`sepae_libera_aluno`.`horario_saida` IS NULL)
+            AND (`turma`.`id` = `aluno`.`turma_id`)
+            AND (`motivo`.`id` = `sepae_libera_aluno`.`motivo_id`)
+            AND (`responsavel_has_aluno`.`aluno_id` = `sepae_libera_aluno`.`aluno_id`)
+            AND (CAST(`sepae_libera_aluno`.`data` AS DATE) = ?)
+            AND (`responsavel_has_aluno`.`responsavel_email` = ?))
+");
+        $consulta->bind_param("ss", $date, $email);
+        $consulta->execute();
+        $resultado = $consulta->get_result();
+        if ($resultado->num_rows > 0) {
+            $resultadoFormatado = $resultado->fetch_all(MYSQLI_ASSOC);
+            $consulta->close();
+            $mysqli->close();
+            return $resultadoFormatado;
+        } else {
+            return false;
+        }
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+        print_r($mysqli->error);
+        exit('Alguma coisa estranha aconteceu...');
+    }
+}
